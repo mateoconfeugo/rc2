9f47b025829db13a8e2f631b4a5697317467f160
Nick Pascucci <npascut1@gmail.com>
2013-09-02 00:52:03 -0400

    Clean up robot.clj and fix a bug.
    When connecting to the robot server, the first packet must be received
    within a fixed timeout. It's a good idea to ping the server as soon as a
    connection is established so that it doesn't time out waiting.

--- src/rc2/lib/robot.clj
+++ src/rc2/lib/robot.clj
@@ -1,7 +1,7 @@
 (ns rc2.lib.robot
   (:use rc2.lib.net
         protobuf.core
-        clojure.string
+        [clojure.string :only (upper-case)]
         clojure.core.async))
 
 (import 'com.pascucci.rc2.Comms$Request)
@@ -13,26 +13,32 @@
 (def Response (protodef Comms$Response))
 (def Waypoint (protodef Comms$Waypoint))
 
-(defn robot-connect! [host port]
-  "Connect to the robot server running on host:port and return a descriptor of the connection."
-  (let [outbound-chan (chan)]
-      {:outbound outbound-chan
-       :inbound (async-protobuf-connection Response host port outbound-chan)}))
-
 (defn- to-enum [sym]
   "Convert a Clojure-style symbol to a protobuf enum symbol. For example, :no-op becomes :NO_OP.
 This is necessary to work around a bug in protobuf.core where symbols in the wrong format are not
 recognized as enum values."
   (keyword (replace (upper-case (name sym)) "-" "_")))
 
-(defn make-waypoint [{:keys [x y z operation] :as waypoint}]
+(defn- make-waypoint [{:keys [x y z operation] :or {x 0 y 0 z 0 operation :no-op}}]
   "Convert a Clojure waypoint into a protocol buffer."
   (assoc (protobuf Waypoint) :x x :y y :z z :operation (to-enum operation)))
 
-(defn make-request [waypoint]
+(defn- make-request [waypoint]
   "Wrap a Clojure waypoint into a request protocol buffer."
   (assoc (protobuf Request) :waypoint (make-waypoint waypoint) :type (to-enum :waypoint)))
 
-(defn robot-exec! [cmds {:keys [outbound] :as conn}]
+(defn ping! [{:keys [outbound] :as conn}]
+  (>!! outbound (assoc (protobuf Request) :type (to-enum :ping)))
+  conn)
+
+(defn send-cmds! [cmds {:keys [outbound] :as conn}]
   "Execute a series of waypoints with the given robot connection."
-  (map #(>!! outbound (make-request %)) cmds))
+  (map #(>!! outbound (make-request %)) cmds)
+  conn)
+
+(defn robot-connect! [host port]
+  "Connect to the robot server running on host:port and return a descriptor of the connection."
+  (let [outbound-chan (chan)
+        connection {:outbound outbound-chan
+                    :inbound (async-protobuf-connection Response host port outbound-chan)}]
+    (ping! connection)))
