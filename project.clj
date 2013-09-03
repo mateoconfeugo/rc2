327907a69b4454ef24b6cad87f093b3704be5924
Nick Pascucci <npascut1@gmail.com>
2013-09-03 01:05:23 -0400

    Update project.clj for driver CLI program.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rc2 "0.1.0-SNAPSHOT"
+(defproject rc2 "0.0.1-SNAPSHOT"
   :description "Command and Control for robots."
   :url "https://nickpascucci.com"
   :license {:name "Eclipse Public License"
@@ -12,4 +12,5 @@
   :plugins [[lein-marginalia "0.7.1"]
             [lein-protobuf "0.3.1"]]
   :proto-path "src/proto"
-  :main rc2.core)
+  :aot [rc2.core rc2.driver]
+  :main rc2.driver)
