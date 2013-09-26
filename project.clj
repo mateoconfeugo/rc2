2b334a6e22ba1ac2fc5a18164782b1f14b4261ef
Nick Pascucci <npascut1@gmail.com>
2013-09-26 00:14:40 -0400

    Remove a bunch of old stuff.

--- project.clj
+++ project.clj
@@ -4,12 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
-                 [protobuf "0.6.2"]
-                 [com.pascucci/s3p "0.1.0-SNAPSHOT"]]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven-local")))
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :plugins [[lein-marginalia "0.7.1"]
-            [lein-protobuf "0.3.1"]]
-  :proto-path "src/proto"
+  :plugins [[lein-marginalia "0.7.1"]]
   :main rc2.core)
--- src/rc2/lib/robot.clj
+++ src/rc2/lib/robot.clj
@@ -1,38 +1,6 @@
-(ns rc2.lib.robot
-  (:use rc2.lib.net
-        protobuf.core
-        clojure.string
-        clojure.core.async))
+(ns rc2.lib.robot)
 
-(import 'com.pascucci.rc2.Comms$Request)
-(import 'com.pascucci.rc2.Comms$Response)
-(import 'com.pascucci.rc2.Comms$Waypoint)
-
-;;;; Functions for interacting with robots.
-(def Request (protodef Comms$Request))
-(def Response (protodef Comms$Response))
-(def Waypoint (protodef Comms$Waypoint))
-
-(defn robot-connect! [host port]
-  "Connect to the robot server running on host:port and return a descriptor of the connection."
-  (let [outbound-chan (chan)]
-      {:outbound outbound-chan
-       :inbound (async-protobuf-connection Response host port outbound-chan)}))
-
-(defn- to-enum [sym]
-  "Convert a Clojure-style symbol to a protobuf enum symbol. For example, :no-op becomes :NO_OP.
-This is necessary to work around behavior in protobuf.core where symbols must match the enum values
-exactly to be recognized."
-  (keyword (replace (upper-case (name sym)) "-" "_")))
-
-(defn make-waypoint [{:keys [x y z operation] :as waypoint}]
-  "Convert a Clojure waypoint into a protocol buffer."
-  (assoc (protobuf Waypoint) :x x :y y :z z :operation (to-enum operation)))
-
-(defn make-request [waypoint]
-  "Wrap a Clojure waypoint into a request protocol buffer."
-  (assoc (protobuf Request) :waypoint (make-waypoint waypoint) :type (to-enum :waypoint)))
-
-(defn robot-exec! [cmds {:keys [outbound] :as conn}]
-  "Execute a series of waypoints with the given robot connection."
-  (map #(>!! outbound (make-request %)) cmds))
+(defprotocol RobotBehavior
+  "State transition functions for a robot"
+  (find-pose [descriptor position] "Perform forward kinematics to find a pose that puts the robot in
+  the given position with the given intrinsic parameters."))
