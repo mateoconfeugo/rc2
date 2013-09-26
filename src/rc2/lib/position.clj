342649fb128299c059a92f96b2a6c86e5ceac875
Nick Pascucci <npascut1@gmail.com>
2013-09-26 00:16:06 -0400

    Add math for calculating positions.

---
+++ src/rc2/lib/position.clj
@@ -0,0 +1,37 @@
+(ns rc2.lib.position)
+
+(defrecord PointCoordinate [x y z])
+
+(defn point-coordinate [x y z]
+  "Get a new point coordinate record for [x,y,z]."
+  (PointCoordinate. x y z))
+
+(def origin (point-coordinate 0 0 0))
+
+;; TODO These have to be defined in a math library somewhere.
+(defn- square [x] (* x x))
+
+(defn- sqrt [x] (Math/sqrt x))
+
+(defn displacement
+  "Find the distance between two points"
+  ([p] (displacement origin p))
+  ([p1 p2] (let [dx (- (:x p1) (:x p2))
+                 dy (- (:y p1) (:y p2))
+                 dz (- (:z p1) (:z p2))]
+               (sqrt (+ (square dx) (square dy) (square dz))))))
+
+(defn rotate [position angle]
+  "Rotate 'position around the Z axis by 'angle radians"
+  (let [ct (Math/cos angle)
+        st (Math/sin angle)
+        {:keys [x y z]} position]
+   (point-coordinate
+    (+ (* ct x) (* st y))
+    (+ (* (- st) x) (* ct y))
+    z)))
+
+(defn within [dist p1 p2]
+  (if (> dist (Math/abs (displacement p1 p2)))
+    true
+    false))
---
+++ test/rc2/lib/position_test.clj
@@ -0,0 +1,28 @@
+(ns rc2.lib.position_test
+  (:use clojure.test
+        rc2.lib.position))
+
+(def tolerance 1e-14)
+
+(deftest test-displacement-default
+  (testing "displacement should return the pythagorean distance between the origin and a point"
+    (is (= 1.0 (displacement (point-coordinate 0 1 0))))
+    (is (= (Math/sqrt 12) (displacement (point-coordinate 2 2 2))))
+    (is (= (Math/sqrt 12) (displacement (point-coordinate -2 -2 -2))))))
+
+(deftest test-displacement-explicit
+  (testing "displacement should return the pythagorean distance between two points"
+    (is (= 1.0 (displacement (point-coordinate 0 1 0) (point-coordinate 0 2 0))))
+    (is (= (Math/sqrt 12) (displacement (point-coordinate 2 2 2) (point-coordinate 4 4 4))))
+    (is (= (Math/sqrt 12) (displacement (point-coordinate -2 -2 -2) (point-coordinate -4 -4 -4))))))
+
+(deftest test-within
+  (testing "within should return true if two points are closer than the given distance from each other"
+    (is (within 1.0 (point-coordinate 0 0 0.5) origin))
+    (is (not (within 1.0 (point-coordinate 0 0 10) origin)))))
+
+(deftest test-rotate
+  (testing "Rotate should rotate the point around the Z axis by the given number of radians"
+    (is (within tolerance (point-coordinate -1.0 0.0 0.0) (rotate (point-coordinate 1.0 0.0 0.0) Math/PI)))
+    (is (within tolerance (point-coordinate 0.0 -1.0 0.0) (rotate (point-coordinate 0.0 1.0 0.0) Math/PI)))
+    (is (within tolerance (point-coordinate 0.0 0.0 1.0) (rotate (point-coordinate 0.0 0.0 1.0) Math/PI)))))
