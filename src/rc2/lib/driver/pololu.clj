01236fbc94215c9fcbef6615391297e7af19738a
Nick Pascucci <npascut1@gmail.com>
2013-09-30 20:41:11 -0400

    Implement duty cycle conversion.

--- src/rc2/lib/driver/pololu.clj
+++ src/rc2/lib/driver/pololu.clj
@@ -1,11 +1,29 @@
 (ns rc2.lib.driver.pololu
+  (:use clojure.tools.trace)
   (:require [rc2.lib.robot :as robot]))
 
-(defrecord PololuInterface [serial])
+(defrecord PololuInterface [serial calibrations])
 
-(defn ->duty-cycle [angle]
-  "Convert an angle to a servo duty cycle in microseconds."
-  0) ;; TODO
+;; Calibration values are given for the low and high microsecond values that the servo can reach,
+;; and the angles produced when these duty cycles are applied to servo. Positive angles are when the
+;; servo arm rotates counter clockwise (when looking at the front/top of the servo) unless :inverted
+;; is true.
+(def default-calibration {:low_usec 985
+                          :high_usec 1995
+                          :low_angle -0.96
+                          :high_angle 0.61
+                          :inverted false})
+
+(defn ->duty-cycle [calibration angle]
+  "Convert 'angle to a servo duty cycle in microseconds, using values in 'calibration."
+  (let [{:keys [low_usec high_usec low_angle high_angle inverted]} calibration]
+    (when (<= low_angle angle high_angle)
+      (let [angle-delta (- angle low_angle)
+            usec-per-rad (/ (- high_usec low_usec) (- high_angle low_angle))
+            usec-delta (* angle-delta usec-per-rad)]
+        (if inverted
+          (- high_usec usec-delta)
+          (+ low_usec usec-delta))))))
 
 (defn move-servo! [servo duty-cycle]
   true) ;; TODO
@@ -13,6 +31,8 @@
 (extend-protocol robot/RobotDriver
   PololuInterface
   (take-pose! [interface pose]
-    (every? true?
-            (map #(move-servo! % (->duty-cycle (% pose)))
-                 (keys (robot/joint-angles pose))))))
+    (let [angles (robot/joint-angles pose)
+          servos (keys angles)]
+      (every? true?
+              (map #(move-servo! % (->duty-cycle (% (:calibrations interface)) (% angles)))
+                   servos)))))
--- test/rc2/lib/driver/pololu_test.clj
+++ test/rc2/lib/driver/pololu_test.clj
@@ -5,10 +5,37 @@
             [rc2.lib.descriptor.delta :as delta]))
 
 (def serial 'serial)
-(def interface (->PololuInterface serial))
+(def calibration {:low_usec 0 :high_usec 100 :low_angle -50 :high_angle 50 :inverted false})
+(def interface (->PololuInterface serial {:a calibration :b calibration :c calibration}))
 (def pose (delta/->DeltaPose {:a 10 :b 5 :c 1}))
 
 (facts "About take-pose!"
-  (fact "take-pose! should return true if all servo operations return true."
+  (fact "take-pose! should return true if all servo operations return true"
     (robot/take-pose! interface pose) => true
-    (provided (move-servo! anything anything) => true :times 3)))
+    (provided (move-servo! anything anything) => true :times 3))
+  (fact "take-pose! should use the calibration value for each servo"
+    (robot/take-pose! (assoc interface :calibrations
+                             {:a (assoc calibration :low_angle 0 :high_angle 20)
+                              :b (assoc calibration :low_angle 0 :high_angle 10)
+                              :c (assoc calibration :low_angle 0 :high_angle 2)}) pose) => true
+    (provided (move-servo! anything 50) => true)))
+
+(facts "About ->duty-cycle"
+  (fact "->duty-cycle should return nil if the angle is higher than the :high_angle"
+    (->duty-cycle calibration 60) => nil)
+  (fact "->duty-cycle should return nil if the angle is lower than the :low_angle"
+    (->duty-cycle calibration -60) => nil)
+  (fact "->duty-cycle should return the midpoint duty cycle for 0 radians"
+    (->duty-cycle calibration 0) => 50)
+  (fact "->duty-cycle should return the low end duty cycle for :low_angle radians"
+    (->duty-cycle calibration -50) => 0)
+  (fact "->duty-cycle should return the high end duty cycle for :high_angle radians"
+    (->duty-cycle calibration 50) => 100)
+  (fact "->duty-cycle should return the high duty cycle for :low_angle radians when inverted"
+    (->duty-cycle (assoc calibration :inverted true) -50) => 100)
+  (fact "->duty-cycle should return the low duty cycle for :high_angle radians when inverted"
+    (->duty-cycle (assoc calibration :inverted true) 50) => 0)
+  (fact "->duty-cycle should return a value below mid for args above mid when inverted"
+    (->duty-cycle (assoc calibration :inverted true) 25) => 25)
+  (fact "->duty-cycle should return a value above mid for args below mid when inverted"
+    (->duty-cycle (assoc calibration :inverted true) -25) => 75))
