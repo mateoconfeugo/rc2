044cdd5071e7f87a23e99df6ea709a3518f5de94
Nick Pascucci <npascut1@gmail.com>
2013-10-19 22:32:01 -0400

    Add Core.Typed annotations to Math

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :url "https://nickpascucci.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[gloss "0.2.2"]
+                 [org.clojure/algo.generic "0.1.1"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.typed "0.2.13"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [serial-port "1.1.2"]
-                 [gloss "0.2.2"]]
+                 [serial-port "1.1.2"]]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven-local")))
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :plugins [[lein-marginalia "0.7.1"]]
--- src/rc2/lib/math.clj
+++ src/rc2/lib/math.clj
@@ -1,18 +1,42 @@
 (ns rc2.lib.math
-  (:require [clojure.math.numeric-tower :as nt]))
+  (:require [clojure.math.numeric-tower :as nt]
+            [clojure.core.typed :as type]))
+
+(type/ann ^:no-check clojure.math.numeric-tower/expt [Number Number -> Number])
+(type/ann ^:no-check clojure.math.numeric-tower/sqrt [Number -> Number])
 
 ;; Constants
+(type/ann pi Double)
 (def pi Math/PI)
 
 ;; Functions
+(type/ann cos [Double -> Number])
 (defn cos [theta] (Math/cos theta))
+
+(type/ann sin [Double -> Number])
 (defn sin [theta] (Math/sin theta))
+
+(type/ann tan [Double -> Number])
 (defn tan [theta] (Math/tan theta))
+
+(type/ann square [Number -> Number])
 (defn square [x] (nt/expt x 2))
+
+(type/ann sqrt [Number -> Number])
 (defn sqrt [x] (nt/sqrt x))
+
+(type/ann abs [Number -> Number])
 (defn abs [x] (nt/abs x))
+
+(type/ann acos [Double -> Number])
 (defn acos [x] (Math/acos x))
+
+(type/ann asin [Double -> Number])
 (defn asin [x] (Math/asin x))
+
+(type/ann atan
+          (Fn [Double -> Number]
+              [Double Double -> Number]))
 (defn atan
   ([x] (Math/atan x))
   ([x y] (Math/atan2 x y)))
