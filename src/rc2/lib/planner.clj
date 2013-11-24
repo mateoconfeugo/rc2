ee0d0310fde7b67bd6916c5a0e36644645ad48a0
Nick Pascucci <npascut1@gmail.com>
2013-11-24 18:47:37 -0500

    Add schema declarations to planner.clj

--- src/rc2/lib/planner.clj
+++ src/rc2/lib/planner.clj
@@ -1,25 +1,24 @@
-(ns rc2.lib.planner)
+(ns rc2.lib.planner
+  (:require [schema.core :as s]))
+
+(def Point [s/Number])
+(def SourceMap {(s/named s/Keyword "part name") Point})
+(def PartList [[(s/named s/Keyword "part name") Point]])
 
 (defn ->source [sources part]
   "Map a part to its source using locations provided in 'sources."
   (when-let [source-location (get sources (first part))]
     (vector source-location (second part))))
 
-(defn map-to-sources [sources destinations]
+(s/defn map-to-sources [sources :- SourceMap
+                        destinations :- [Point]]
   "Map destinations to their component sources."
   (map (partial ->source sources) destinations))
 
 (defn plan-pick-and-place [sources destinations]
   "Plan a pick and place sequence to populate the positions in 'destinations using components from
   'sources."
-  ;; The basic algorithm goes like this:
-  ;; 1. Join each destination point with the location of its component source.
   (let [source-paths (map-to-sources sources destinations)]
-    ;; 2. Permute the point list to minimize the distance between a destination and the next source.
-    ;; This is done using the following algorithm:
-    ;; Compute distances between each destination and each source. Sort the points by these
-    ;; distances, and select the lowest weighted path. Add the path to the path list. Select the
-    ;; next lowest path - if it's from a destination that's already in the path list, skip it. If
-    ;; not, add it to the path list. Repeat untill all destinations have been accounted for.
     (when (not-any? nil? source-paths)
+      ;; TODO Optimize the path.
       (apply concat source-paths))))
