c4df07ac105828b4d04077bda06db1a6f219b442
Nick Pascucci <npascut1@gmail.com>
2013-11-24 18:49:36 -0500

    Add option to use source/dest files.

--- src/rc2/core.clj
+++ src/rc2/core.clj
@@ -4,6 +4,7 @@
            [rc2.lib
             [math :as math]
             [position :as pos]
+            [planner :as plan]
             [robot :as rbt]]
            [rc2.lib.descriptor.delta :as delta]
            [rc2.lib.driver.pololu :as pol]
@@ -18,13 +19,15 @@
   "Parse command line arguments into a map of their values."
   (cli/cli args
            ["-p" "--port" "Serial port to use"]
-           ["-f" "--targets-file" "List of positions to move to"]))
+           ["-f" "--targets-file" "List of positions to move to"]
+           ["-s" "--source-file" "List of part sources"]
+           ["-d" "--destination-file" "List of part destinations"]))
 
 (defn connect [port & {:keys [descriptor calibrations]
                        :or {descriptor (delta/->DeltaDescriptor 10.2 15 3.7 4.7)
                             calibrations {:a pol/default-calibration
-                                           :b pol/default-calibration
-                                           :c pol/default-calibration}}}]
+                                          :b pol/default-calibration
+                                          :c pol/default-calibration}}}]
   "Connect to a robot and return a map of connection parameters."
   (let [serial-port (serial/open port)]
     (assoc {}
@@ -42,8 +45,8 @@
         take-pose! (partial rbt/take-pose! interface)]
     (doseq [p intermediates]
       (if-let [pose (find-pose p)]
-       (take-pose! pose)
-       (println "ERROR: Position " p " was not reachable."))
+        (take-pose! pose)
+        (println "ERROR: Position " p " was not reachable."))
       (Thread/sleep (if interpolate interpolate-delay no-interpolate-delay)))
     (swap! last-move (constantly pos))))
 
@@ -62,14 +65,23 @@
          (= :tool type) (apply change-tool-state! params)
          :else (println "Unknown task" task))))))
 
+(defn find-path [source-file destination-file]
+  "Find a path which populates all of the part locations in 'destination-file with parts from the
+   locations in 'source-file."
+  (let [sources (load-file source-file)
+        destinations (load-file destination-file)]
+    (plan/plan-pick-and-place sources destinations)))
 
 (def max-velocity (/ math/pi 40))
 (def max-accel (/ math/pi 40))
 (defn -main [& args]
   (println "Starting up.")
-  (let [{:keys [port targets-file]} (first (parse-args args))
+  (let [{:keys [port targets-file source-file destination-file]} (first (parse-args args))
         {:keys [serial-port descriptor interface]} (connect port)
-        positions (load-file targets-file)]
+        positions (if targets-file (do (println "Using targets from " targets-file)
+                                       (load-file targets-file))
+                      (do (println "Planning path using " source-file destination-file)
+                          (find-path source-file destination-file)))]
     (println "Port: " port)
     (rbt/initialize! interface)
     ;; TODO Extract this into a macro that executes a user script.
