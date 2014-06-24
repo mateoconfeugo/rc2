fa5dfa2dcc2d37424f3c6d71fe51e78db649f6b7
Nick Pascucci <npascut1@gmail.com>
2014-06-23 20:11:27 -0400

    Update README and license.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # RC2
 
-A command and control (C2) program for robots. The successor of delta_gui.
+The name has two meanings: First, it's a command and control (C2) program for [R]obots; it's also a
+rewrite of an earlier project in Clojure (Robot Controller #2).
 
 ## Usage
 
-FIXME
+This project is currently (June 2014) in an intermediate phase - I'm rewriting it to have a
+web-based UI which lets you control the robot interactively. You can use some of the functionality
+from the REPL, and earlier revisions would let you specify a script to run.
 
 ## License
 
 Copyright © 2013 Nick Pascucci
 
-Distributed under the Eclipse Public License, the same as Clojure.
+Distributed under the GPL V2.
--- project.clj
+++ project.clj
@@ -1,24 +1,41 @@
 (defproject rc2 "0.1.0-SNAPSHOT"
   :description "Command and Control for robots."
   :url "https://nickpascucci.com"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License V2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [
                  [gloss "0.2.2"]
                  [org.clojure/algo.generic "0.1.1"]
                  [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2234"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]
                  [org.clojure/core.typed "0.2.13"]
                  [org.clojure/math.combinatorics "0.0.7"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/tools.cli "0.2.4"]
+                 [prismatic/dommy "0.1.2"]
                  [prismatic/schema "0.1.8"]
                  [serial-port "1.1.2"]
                  ]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven-local")))
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :plugins [[lein-marginalia "0.7.1"]]
+  :plugins [
+            [lein-cljsbuild "1.0.3"]
+            [lein-marginalia "0.7.1"]
+            ]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.1"]]}}
+  :cljsbuild {
+              :builds {:release {:source-paths ["src-cljs"]
+                                 :compiler {:output-to "resources/public/js/rc2.js"
+                                            :optimizations :whitespace
+                                            :pretty-print true}
+                                 :jar true}
+                       :test {:source-paths ["src-cljs" "test-cljs"]
+                              :compiler {:output-to "target/unit-test.js"
+                                         :optimizations :whitespace
+                                         :pretty-print true}
+                              :notify-command ["resources/specljs-runner.js"
+                                               "target/unit-test.js"]}}}
   :core.typed {:check [rc2.lib.math rc2.lib.position]}
   :main rc2.core)
