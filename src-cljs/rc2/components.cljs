ce610bdc2adfd71814c7d0141860b9adaabd4572
Nick Pascucci <npascut1@gmail.com>
2014-12-16 01:59:37 -0500

    Move components to their own namespace.

---
+++ src-cljs/rc2/components.cljs
@@ -0,0 +1,70 @@
+(ns rc2.components
+  (:use-macros [dommy.macros :only [sel1]])
+  (:require [reagent.core :as reagent]
+            [rc2.api :as api]
+            [rc2.draw :as draw]
+            [rc2.state :as state]))
+
+(defn- label
+  "A simple label that contains text."
+  ([id text] (label id text ""))
+  ([id text classes]
+     [:div.label {:id id :class classes} text]))
+
+(defn- lighter [{:keys [primary secondary]}]
+  "A mode information label showing primary and secondary modes."
+  (let [primary-text (.-lighter primary)
+        secondary-text (.-lighter secondary)
+        text (str secondary-text " " primary-text " MODE")]
+    [label "lighter" text]))
+
+(defn- main-button [id text]
+  "Clickable button element triggers state changes on click."
+  [:button.ui-element
+   {:id id
+    :on-mouse-down #(state/on-button-click! id :down)
+    :on-mouse-up #(state/on-button-click! id :up)} text])
+
+(defn- section-item [text highlight]
+  "A list item in a section."
+  (let [class (if highlight "highlight" "")]
+    [:li {:class class} text]))
+
+(defn- section [id title items xform]
+  "Component which displays a list of items with a title."
+  [:div.ui-element.section {:id id}
+   [:span.section-title title]
+   [:ul (for [item items]
+          [section-item (str (xform item)) (:highlight item)])]])
+
+(defn- visualizer []
+  "HTML5 canvas element which serves as a draw target for the route visualization."
+  (let [canvas (sel1 :#target)]
+    (draw/draw canvas @state/app-state))
+  [:div "[placeholder]"])
+
+(defn ui-elements []
+  "Top-level RC2 UI element."
+  (let [app-state @state/app-state]
+    [:div
+     [section "waypoint-list" "WAYPOINTS"
+      (draw/indexed (get-in app-state [:route :waypoints]))
+      (partial draw/get-waypoint-text (get app-state :parts)
+               (get-in app-state [:route :execution :current]))]
+     [section "part-list" "PARTS"
+      (sort-by :id (map (fn [[id part]] (assoc part :id id)) (get app-state :parts)))
+      (fn [part] (str (:id part) ": " (:name part)))]
+     [section "plan-list" "PLAN"
+      (draw/indexed (get-in app-state [:route :plan]))
+      (partial draw/get-waypoint-text (get app-state :parts)
+               (get-in app-state [:route :execution :current]))]
+     [:div#main-buttons (for [[id button] (filter (fn [[id btn]]
+                                                    (when-let [pred (:visible-when btn)]
+                                                      (pred app-state)))
+                                                  (get-in app-state [:ui :buttons]))]
+                          [main-button id (:text button)])]
+     [lighter (:mode app-state)]
+     [label "connection"
+      (if (:connected (:connection app-state)) "CONNECTED" "OFFLINE")
+      (if (:connected (:connection app-state)) "normal" "error")]
+     [label "time" (str (:time app-state))]]))
--- src-cljs/rc2/main.cljs
+++ src-cljs/rc2/main.cljs
@@ -3,7 +3,8 @@
   (:require [reagent.core :as reagent]
             [rc2.api :as api]
             [rc2.draw :as draw]
-            [rc2.state :as state]))
+            [rc2.state :as state]
+            [rc2.components :as components]))
 
 (def timer-id (atom nil))
 (def framerate 40)
@@ -13,63 +14,6 @@
   (state/update-periodic-tasks!)
   (state/on-state-change!))
 
-(defn label
-  ([id text] (label id text ""))
-  ([id text classes]
-     [:div.label {:id id :class classes} text]))
-
-(defn lighter [{:keys [primary secondary]}]
-  (let [primary-text (.-lighter primary)
-        secondary-text (.-lighter secondary)
-        text (str secondary-text " " primary-text " MODE")]
-    [label "lighter" text]))
-
-(defn visualizer []
-  (let [canvas (sel1 :#target)]
-    (draw/draw canvas @state/app-state))
-  [:div "[placeholder]"])
-
-(defn main-button [id text]
-  [:button.ui-element {:id id
-                       :on-mouse-down #(state/on-button-click! id :down)
-                       :on-mouse-up #(state/on-button-click! id :up)} text])
-
-(defn section-item [text highlight]
-  (let [class (if highlight "highlight" "")]
-    [:li {:class class} text]))
-
-(defn section [id title items xform]
-  "Component which displays a list of items with a title."
-  [:div.ui-element.section {:id id}
-   [:span.section-title title]
-   [:ul (for [item items]
-          [section-item (str (xform item)) (:highlight item)])]])
-
-(defn ui-elements []
-  (let [app-state @state/app-state]
-    [:div
-     [section "waypoint-list" "WAYPOINTS"
-           (draw/indexed (get-in app-state [:route :waypoints]))
-           (partial draw/get-waypoint-text (get app-state :parts)
-                    (get-in app-state [:route :execution :current]))]
-     [section "part-list" "PARTS"
-      (sort-by :id (map (fn [[id part]] (assoc part :id id)) (get app-state :parts)))
-      (fn [part] (str (:id part) ": " (:name part)))]
-     [section "plan-list" "PLAN"
-      (draw/indexed (get-in app-state [:route :plan]))
-      (partial draw/get-waypoint-text (get app-state :parts)
-               (get-in app-state [:route :execution :current]))]
-     [:div#main-buttons (for [[id button] (filter (fn [[id btn]]
-                                                    (when-let [pred (:visible-when btn)]
-                                                              (pred app-state)))
-                                                  (get-in app-state [:ui :buttons]))]
-                          [main-button id (:text button)])]
-     [lighter (:mode app-state)]
-     [label "connection"
-      (if (:connected (:connection app-state)) "CONNECTED" "OFFLINE")
-      (if (:connected (:connection app-state)) "normal" "error")]
-     [label "time" (str (:time app-state))]]))
-
 (defn main []
   (api/get-meta (fn [data] (.log js/console "API server uptime:" (get data "uptime")))
                 (fn [err] (.log js/console "Error getting server metadata:" (str err))))
@@ -78,6 +22,6 @@
         canvas (sel1 :#target)]
     (state/attach-handlers body canvas)
     (state/on-state-change!))
-  (reagent/render-component [ui-elements] (sel1 :#ui-elements))
-  (reagent/render-component [visualizer] (sel1 :#placeholder))
+  (reagent/render-component [components/ui-elements] (sel1 :#ui-elements))
+  (reagent/render-component [components/visualizer] (sel1 :#placeholder))
   (swap! timer-id #(.setInterval js/window on-timer-tick! (/ 1000 framerate))))
